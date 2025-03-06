9d3cc2f7a55f989b7b4fb028616ec62c25e6c6ef
Moorits Mihkel Muru <mmuru@aip.de>
2025-03-06 16:10:04 +0100

    feat: add reading of simulation output from binary files

--- src/extracthaloparticles.jl
+++ src/extracthaloparticles.jl
@@ -20,9 +20,9 @@ function read_halo_particles_IDs(haloID::String, simspecs::SimulationSpecs)::Dat
                 println("Found specified halo")
                 println("N_particles haloID")
                 println(line)
-                
+
                 n_part = parse(Int, split(line)[1])
-                
+
                 sizehint!(halo_particle_IDs, n_part)
                 sizehint!(halo_particle_types, n_part)
 
@@ -41,7 +41,72 @@ function read_halo_particles_IDs(haloID::String, simspecs::SimulationSpecs)::Dat
 end
 read_halo_particles_IDs(haloID::Int, simspecs::SimulationSpecs) = read_halo_particles_IDs(string(haloID), simspecs)
 
-function read_particle_data(halo_particles::DataFrame, simspecs::SimulationSpecs)
+function read_particle_data_binary(halo_particles::DataFrame, simspecs::SimulationSpecs)
+    particle_files = get_simparticle_filepaths(simspecs)
+    pids_set = Set(halo_particles.pid) |> (ids -> convert.(UInt32, ids))
+    println("Looking for $(size(halo_particles, 1)) DM particles")
+
+    particles_dict = Dict(1 => DataFrame())
+
+    read_blocksize(io::IO) = read(io, Int32)
+    check_block_end(blocksize_init, blocksize_final) = blocksize_init == blocksize_final || error("Incorrect reading of data block.")
+
+    for particle_file in particle_files
+        if isempty(pids_set)
+            println("All particles found, skipping the rest of the files")
+            break
+        end
+        println("Reading $(split(particle_file, "/")[end])")
+        # Binary file is read according the specification of GADGET-2 user guide section 6
+        # https://wwwmpa.mpa-garching.mpg.de/gadget/users-guide.pdf
+        open(particle_file) do file
+            file_dict = Dict()
+            blocksize = read_blocksize(file)
+            f_pos = position(file)
+            # NOTE: Could make reading faster by only reading the high resolution particles
+            # specified in n_particles[2] and then skipping the rest of the particles using
+            # seek(file, f_pos + blocksize) and then proceeding to the next block
+            n_particles = read!(file, Array{UInt32}(undef, 6))
+            if n_particles[2] == 0
+                println("No DM particles in this file")
+                continue
+            end
+            seek(f, f_pos + blocksize)
+            check_block_end(blocksize, read_blocksize(file))
+
+            blocksize = read_blocksize(file)
+            file_dict["Coordinates"] = read!(file, Array{Float32}(undef, 3, blocksize÷(4*3)))
+            check_block_end(blocksize, read_blocksize(file))
+
+            blocksize = read_blocksize(file)
+            file_dict["Velocities"] = read!(file, Array{Float32}(undef, 3, blocksize÷(4*3)))
+            check_block_end(blocksize, read_blocksize(file))
+
+            blocksize = read_blocksize(file)
+            file_dict["ParticleIDs"] = read!(file, Array{UInt32}(undef, blocksize÷4))
+            check_block_end(blocksize, read_blocksize(file))
+
+            blocksize = read_blocksize(file)
+            file_dict["Masses"] = read!(file, Array{Float32}(undef, blocksize÷4))
+            check_block_end(blocksize, read_blocksize(file))
+        end
+
+        mask = zeros(Bool, length(file_dict["ParticleIDs"]))
+        tmap!(in(pids_set), mask, file_dict["ParticleIDs"])
+        println("Found $(sum(mask)) DM particles")
+        setdiff!(pids_set, file_dict["ParticleIDs"][mask]) # Remove the found particles
+
+        update_particles_dict!(particles_dict, file_dict, mask, 1)
+    end
+
+    println("Extracted $(size(particles_dict[1], 1)) DM particles")
+    println("Difference from expected: $(size(particles_dict[1], 1) - size(halo_particles, 1))")
+
+    println()
+    convert_particles_dict_to_df(particles_dict)
+end
+
+function read_particle_data_hdf5(halo_particles::DataFrame, simspecs::SimulationSpecs)
     particle_types = [0, 1, 4, 5]
     particles_dict = Dict(type => DataFrame() for type in particle_types)
     halo_particles_grouped = groupby(halo_particles, :ptype)
@@ -75,7 +140,7 @@ function read_particle_data(halo_particles::DataFrame, simspecs::SimulationSpecs
                     rethrow(e)
                 end
             end
-            
+
             all_ids = read(particles, "PartType$(particle_type)/ParticleIDs")
             mask = zeros(Bool, length(all_ids))
             tmap!(in(halo_ids), mask, all_ids)
@@ -83,7 +148,7 @@ function read_particle_data(halo_particles::DataFrame, simspecs::SimulationSpecs
 
             group_dict = read(particles, "PartType" * string(particle_type))
             update_particles_dict!(particles_dict, group_dict, mask, particle_type)
-        
+
         end
 
         close(particles)
@@ -99,6 +164,7 @@ function read_particle_data(halo_particles::DataFrame, simspecs::SimulationSpecs
     println()
     convert_particles_dict_to_df(particles_dict)
 end
+read_particle_data(halo_particles::DataFrame, simspecs::SimulationSpecs) = simspecs.galfor ? read_particle_data_hdf5(halo_particles, simspecs) : read_particle_data_binary(halo_particles, simspecs)
 read_particle_data(haloID::Union{Int, String}, simspecs::SimulationSpecs) = read_particle_data(read_halo_particles_IDs(haloID, simspecs), simspecs)
 
 function update_particles_dict!(particles_dict::Dict{Int64, DataFrame}, group_dict::Dict, mask::Vector{Bool}, particle_type::Int64)
@@ -111,7 +177,7 @@ function update_particles_dict!(particles_dict::Dict{Int64, DataFrame}, group_di
             delete!(group_dict, prop_key)
         end
     end
-    
+
     particles_dict[particle_type] = vcat(
         particles_dict[particle_type],
         DataFrame(group_dict)[mask, :] # Only write the lines that are in the mask
@@ -132,6 +198,10 @@ function convert_particles_dict_to_df(particles_dict::Dict{Int64, DataFrame})
         length_df = size(temp_df, 1)
         temp_df.ptype = fill(particle_types_dict[ptype_num], length_df)
         append!(particles_df, temp_df; cols = :union)
+    end
+
+    if !(:GFM_StellarFormationTime in names(particles_df))
+        particles_df.GFM_StellarFormationTime = fill(missing, size(particles_df, 1))
     end
 
     particles_df
